7734545d69bdf3a44756d4b79725364909a732a9
Fantech <fantech348@gmail.com>
2026-01-12 15:43:01 +0200

    Add files via upload

---
+++ scripts/unlock.js
@@ -0,0 +1,4 @@
+Planets.gier.accessible = Planets.gier.alwaysUnlocked = true
+Planets.notva.accessible = Planets.notva.alwaysUnlocked = true
+Planets.verilus.accessible = Planets.verilus.alwaysUnlocked = true
+Log.info("all asteroids are unlocked :D")
